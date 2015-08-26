c9a8cc58f15a3b2900aebc173390ede970b836a2
Rodrigo Gularte Merida <rodrigo.gularte@ulg.ac.be>
2015-08-26 12:58:26 +0200

    modified:   src/README.md
    renamed:    src/cf2_submit.slq -> src/cf2_submit_world.slq
    renamed:    src/cf2_analyze_q1_world.R -> src/cf2_analyze_world.R
    renamed:    src/cf2_make_q1_world.R -> src/cf2_make_world.R
    
    new file:   src/cf2_analyze_cross.i.R
    new file:   src/cf2_submit_cross.i.slq
    new file:   src/cf2_submit_make_world.slq

---
+++ src/cf2_analyze_cross.i.R
@@ -0,0 +1,64 @@
+## Analyze 1000*4*4 crosses from 1 chr with 2 QTL spaced at 15, 30 and 45 cM appart
+## with varying a and d effect in a semi factorial design
+## Setting up a working directory for the QTL effects 1
+setwd("./cf2_o")
+
+## error options
+options(error = quote(dump.frames("q1_A3", to.file = TRUE)))
+
+
+library(qtl)
+library(RColorBrewer)
+
+sapply(file.path("../src/", c("FileCheck.R", "mcim.R", "SelectMarkers.R",
+                 "sim.cross.f2.npheno.R", "sim.cross.npheno.R", "plotmedians.R",
+                 "plotmedians2.R", "LinkageAnalysis.R", "StepwiseAnalysis.R")),
+       function(f) {
+           source(paste(f, sep = ""))
+       })
+
+### FILE NAME ###
+args <- commandArgs(trailingOnly = TRUE)
+( cross.i <- as.character(args[1]) )
+
+cross.no <- strsplit(cross.i, split = "_")[[1]][1]
+random.sample = readLines("RandomSample.txt")
+
+
+##==============================================================================
+
+cross.a <- read.cross(format = "csv", dir = "../cf2_d", file = cross.i)
+
+cross.a <- est.rf(cross.a)
+cross.a <- calc.genoprob(cross.a)
+phenos <- names(cross.a$pheno)
+
+im.hk.a <- scanone(cross.a, pheno.col = phenos, method = "hk")
+cim.hk.a <- mcim(cross.a, pheno.col = phenos, method = "hk")
+stq.hk.a <- stepwiseqtl.mt(cross.a, pheno.list = phenos, method = "hk", max.qtl = 4)
+
+smry.im.hk.a <- sumarize.scanone(im.hk.a, cross.no = cross.no, cross = cross.a, pheno.list = phenos,
+                                 k.dist = 15, mapped.with = "IM")
+
+smry.cim.hk.a <- sumarize.scanone(cim.hk.a, cross.no = cross.no, cross = cross.a, pheno.list = phenos,
+                                  k.dist = 15, mapped.with = "CIM")
+
+smry.stq.hk.a <- summary.mtqtl(stq.hk.a, cross.no = cross.no, cross = cross.a, pheno.list = phenos,
+                               k.dist = 15, mapped.with = "stq")
+
+write.csv(rbind(smry.im.hk.a, smry.cim.hk.a),
+          file = paste(gsub("\\.csv", "", cross.i), "_S1smry.csv", sep = ""),
+          row.names = FALSE, append = TRUE)
+
+write.csv(smry.stq.hk.a, file = paste(gsub("\\.csv", "", cross.i), "_MQMsmry.csv", sep = ""),
+          row.names = FALSE, append = TRUE)
+
+save.image(gsub("csv", "rda", cross.i))
+
+if(cross.no %in% random.sample) save(list = ls(pattern = "a"),
+                                     file = paste("../cf2_e/", gsub("csv", "RData", cross.i), sep = ""))
+
+system(paste("mv ../cf2_d/", cross.i, " ../cf2_p", sep = ""))
+
+##==============================================================================
+
